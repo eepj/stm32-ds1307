26753bd385e9655b9cf3c6debb20f3c5adae8599
Anonymous <62833867+eepj@users.noreply.github.com>
2020-04-26 00:08:41 +0800

    Update main.c

--- examples/main.c
+++ examples/main.c
@@ -19,8 +19,8 @@
  *								 _________
  *		32.768 kHz	XTAL1	1 --| o       |-- 8 VCC (3V3)
  *					XTAL2	2 --|         |-- 7 N/C
- *					N/C		3 --|         |-- 6 SCL (PB6 for STM32F030K6)
- *					GND		4 --|_________|-- 7 SDA (PB7 for STM32F030K6)
+ *					N/C		3 --|         |-- 6 SCL (PB6 for STM32F030x6/8/C)
+ *					GND		4 --|_________|-- 7 SDA (PB7 for STM32F030x6/8/C)
  *
  *				Refer to datasheets for further information.
  *
